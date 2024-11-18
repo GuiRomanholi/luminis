b9d80057df0fe7bc5f31e8c10e8c17e31b21e092
GuiRomanholi <guilhermeroma343@gmail.com>
2024-11-18 09:11:18 -0300

    Service Usuario feito

---
+++ src/main/java/br/com/luminis/service/UsuarioService.java
@@ -0,0 +1,50 @@
+package br.com.luminis.service;
+
+import br.com.luminis.dao.UsuarioDao;
+import br.com.luminis.dto.UsuarioRequestDto;
+import br.com.luminis.models.Usuario;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsuarioService {
+
+    private UsuarioDao usuarioDao = new UsuarioDao();
+
+    public void cadastrar(UsuarioRequestDto usuarioDto){
+        Usuario usuario = usuarioDto.convert(usuarioDto);
+        usuarioDao.cadastrarUsuario(usuario);
+    }
+
+    public List<UsuarioRequestDto> listar(){
+        List<Usuario> produtos = usuarioDao.buscarTodosUsuarios();
+        return produtos.stream()
+                .map(usuario -> {
+                    UsuarioRequestDto usuarioDto = new UsuarioRequestDto();
+                    usuarioDto.setId_usu(usuario.getId_usu());
+                    usuarioDto.setNome(usuario.getNome());
+                    usuarioDto.setSenha(usuario.getSenha());
+                    usuarioDto.setEmail(usuario.getEmail());
+                    return usuarioDto;
+                }).collect(Collectors.toList());
+    }
+
+    public void deletar(String id){
+        usuarioDao.deletar(id);
+    }
+
+    public UsuarioRequestDto buscarPorId(String id) {
+        Usuario usuario = usuarioDao.buscarPorId(id);
+        UsuarioRequestDto usuarioRequestDto = new UsuarioRequestDto();
+        usuarioRequestDto.setId_usu(usuario.getId_usu());
+        usuarioRequestDto.setNome(usuario.getNome());
+        usuarioRequestDto.setSenha(usuario.getSenha());
+        usuarioRequestDto.setEmail(usuario.getEmail());
+        return usuarioRequestDto;
+    }
+
+    public void atualizar(UsuarioRequestDto produtoDto) {
+        Usuario produto = produtoDto.convert(produtoDto);
+        usuarioDao.alterar(produto);
+    }
+}
